5fa80197a82591b425f2ba9d02703667bf93a9db
Zhaokun Qin <qinzhaokun@qq.com>
2015-12-12 13:21:02 +0800

    Create 226_Easy_InvertBinaryTree.java

---
+++ 226_Easy_InvertBinaryTree.java
@@ -0,0 +1,35 @@
+/*
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root != null){
+            TreeNode tmp = invertTree(root.left);
+            root.left = invertTree(root.right);
+            root.right = tmp;
+        }
+        return root;
+    }
+}
