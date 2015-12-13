aad5e7bb800904aa87ad78de3caf719cee1453e3
Zhaokun Qin <qinzhaokun@qq.com>
2015-12-13 11:10:42 +0800

    Create 231_Easy_PowerofTwo.java

---
+++ 231_Easy_PowerofTwo.java
@@ -0,0 +1,19 @@
+/*
+Given an integer, write a function to determine if it is a power of two.
+*/
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        int num = 1;
+        for(int i = 0;i < 32;i++){
+            if(n == num){
+                return true;
+            }
+            else if(n < num){
+                return false;
+            }
+            num = num << 1;
+        }
+        return false;
+    }
+}
