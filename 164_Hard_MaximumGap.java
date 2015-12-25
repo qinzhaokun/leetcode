39635edf9c48dc032d06e80f9f010b5c21b54460
Zhaokun Qin <qinzhaokun@qq.com>
2015-12-25 10:45:45 +0800

    Update 164_Hard_MaximumGap.java

--- 164_Hard_MaximumGap.java
+++ 164_Hard_MaximumGap.java
@@ -8,6 +8,7 @@ Return 0 if the array contains less than 2 elements.
 You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
 */
 
+//bucket sort
 public class Solution {
     public int maximumGap(int[] nums) {
         if(nums.length < 2){
@@ -50,4 +51,29 @@ public class Solution {
 		    }
 		return maxGap;
   }
+}
+
+//treeMap
+public class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length < 2){
+            return 0;
+        }
+        TreeMap<Integer, Integer> treeMap = new TreeMap<Integer, Integer>();
+        for(int i = 0 ;i < nums.length;i++){
+            if(!treeMap.containsKey(nums[i])){
+                treeMap.put(nums[i],1);
+            }
+        }
+        Iterator it = treeMap.entrySet().iterator();
+        int last =(int)((Map.Entry) it.next()).getKey();
+        int max = 0;
+		  while (it.hasNext()) {
+		     Map.Entry entry =(Map.Entry) it.next();
+    		 int key = (int)entry.getKey();
+             max = Math.max(max,key-last);
+             last = key;
+          }
+          return max;
+    }
 }
