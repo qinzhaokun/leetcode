54b2e733c92de8d449fc2c27056b2d3646a66a8c
Zhaokun Qin <qinzhaokun@qq.com>
2016-03-30 13:58:28 +0800

    Create 190_Easy_ReverseBits.java

---
+++ 190_Easy_ReverseBits.java
@@ -0,0 +1,34 @@
+/*
+Reverse bits of a given 32 bits unsigned integer.
+
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+Follow up:
+If this function is called many times, how would you optimize it?
+*/
+
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int re = 0;
+        for(int i = 0;i < 32;i++){
+            re = re << 1;
+            re = re | (n & 1);
+            n = n >> 1;
+        }
+        return re;
+    }
+}
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
+};
