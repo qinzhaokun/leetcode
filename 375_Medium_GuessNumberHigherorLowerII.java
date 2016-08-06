2b73ce72758c4196c773530d4d79bcff038cdb6c
Zhaokun Qin <qinzhaokun@qq.com>
2016-08-06 11:36:44 +0800

    Create 375_Medium_GuessNumberHigherorLowerII.java

---
+++ 375_Medium_GuessNumberHigherorLowerII.java
@@ -0,0 +1,50 @@
+/*
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number I picked is higher or lower.
+
+However, when you guess a particular number x, and you guess wrong, you pay $x. You win the game when you guess the number I picked.
+
+Example:
+
+n = 10, I pick 8.
+
+First round:  You guess 5, I tell you that it's higher. You pay $5.
+Second round: You guess 7, I tell you that it's higher. You pay $7.
+Third round:  You guess 9, I tell you that it's lower. You pay $9.
+
+Game over. 8 is the number I picked.
+
+You end up paying $5 + $7 + $9 = $21.
+Given a particular n ≥ 1, find out how much money you need to have to guarantee a win.
+*/
+
+public class Solution {
+    public int getMoneyAmount(int n) {
+        int [][] dp = new int [n+2][n+2];
+        
+        for(int i = 1;i <= n;i++) {
+            dp[i][i] = 0;
+            if(i != n)
+                dp[i][i+1] = i;
+        }
+        
+        for(int l = 2;l <= n;l++){
+            for(int i = 1;i+l <= n;i++){
+                int left = i;int right = i+l;
+                int min = Integer.MAX_VALUE;
+                for(int j = left;j <= right;j++){
+                    int target = Math.max(dp[left][j-1],dp[j+1][right]);
+                    if(min > target + j){
+                        min = target + j;
+                    }
+                }
+                dp[left][right] = min;
+            }
+        }
+        
+        return dp[1][n];
+    }
+}
